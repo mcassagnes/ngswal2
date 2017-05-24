e842fea040874ef6e7346ede6ca4950bcefc3bc3
Mathieu Cassagnes <mcassagnes@gmail.com>
2017-05-23 21:18:50 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import angular from 'angular'
+import swal from 'sweetalert2'
+
+const ngSwalModule = angular.module('ngSwal2', [])
+
+.factory('Swal', [function ($q, $rootScope) {
+  function NgSwal (options) {
+    return $q((resolve, reject) => {
+      swal(options).then(resolve).catch(reject)
+    })
+  }
+  return NgSwal
+}])
+
+.name
+
+export default ngSwalModule
