9b73e766803198c7d8f747d1e5fed40ffa0d5586
Mathieu Cassagnes <mcassagnes@gmail.com>
2017-05-23 21:34:22 -0400

    Fixed DI

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import swal from 'sweetalert2'
 
 const ngSwalModule = angular.module('ngSwal2', [])
 
-.factory('Swal', [function ($q, $rootScope) {
+.factory('Swal', ['$q', '$rootScope', function ($q, $rootScope) {
   function NgSwal (options) {
     return $q((resolve, reject) => {
       swal(options).then(resolve).catch(reject)
